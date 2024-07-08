bbf5e37fbb5fa7e02828ce17dd555ca1453cb982
DerzoGit <journetalexandre.contact@gmail.com>
2024-07-08 20:41:05 +0200

    product model

---
+++ backend/models/product.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize')
+
+// Modèle Product
+module.exports = (sequelize) => {
+    const Product = sequelize.define("Product", {
+        id: { type: DataTypes.INTEGER(10), primaryKey: true, autoIncrement: true },
+        userId: { type: DataTypes.INTEGER(10), allowNull: false },
+        nom: { type: DataTypes.STRING, allowNull: false },
+        description: { type: DataTypes.TEXT, allowNull: false },
+        image: { type: DataTypes.STRING, allowNull: false }
+    }, {
+        paranoid: true // Soft delete
+    })
+
+    return Product
+}
