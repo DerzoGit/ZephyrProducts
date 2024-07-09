06e9fd49cffa2f7d5daaca018e0453b6135939b2
DerzoGit <journetalexandre.contact@gmail.com>
2024-07-09 19:33:15 +0200

    login ratelimiter

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,12 +2,14 @@
 const express = require('express')
 // Import du contrôleur d'authentification
 const authCtrl = require('../controllers/auth')
+// Import du rateLimiter
+const { loginLimiter } = require('../middleware/rateLimiter')
 
 // Récupération routage express
 let router = express.Router()
 
 // Routage Auth
 router.put("/signup", authCtrl.signup)
-router.post("/login", authCtrl.login)
+router.post("/login", loginLimiter, authCtrl.login)
 
 module.exports = router
