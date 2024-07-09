f760772b27fc2e08b7ee1ef893c149e72d6b57e8
DerzoGit <journetalexandre.contact@gmail.com>
2024-07-09 13:56:32 +0200

    nom produit unique, del unique userId

--- backend/models/product.js
+++ backend/models/product.js
@@ -4,8 +4,8 @@ const { DataTypes } = require('sequelize')
 module.exports = (sequelize) => {
     const Product = sequelize.define("Product", {
         id: { type: DataTypes.INTEGER(10), primaryKey: true, autoIncrement: true },
-        userId: { type: DataTypes.INTEGER(10), allowNull: false, unique: true },
-        nom: { type: DataTypes.STRING, allowNull: false },
+        userId: { type: DataTypes.INTEGER(10), allowNull: false },
+        nom: { type: DataTypes.STRING, allowNull: false, unique: true },
         description: { type: DataTypes.TEXT, allowNull: false }
     }, {
         paranoid: true // Soft delete
